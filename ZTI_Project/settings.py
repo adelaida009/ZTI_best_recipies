69446f4590f52e57cfcfd71dcfab686e46a7dc70
Nero515 <rem.pomorski@gmail.com>
2021-04-17 01:43:17 +0200

    Dodanie modelu Usera i podłączenie bazy mongoDB

--- ZTI_Project/settings.py
+++ ZTI_Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from urllib.parse import quote_plus
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "recipes.apps.RecipesConfig",
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -74,11 +84,16 @@ WSGI_APPLICATION = 'ZTI_Project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+"default": {
+    "ENGINE": "djongo",
+    "CLIENT": {
+        "host": "mongodb+srv://zti2021.dv9yv.mongodb.net/myFirstDatabase",
+        "username": "RemekiAga",
+        "password": "RemekiAga",
+        "name": "ZTI",
+        "authMechanism": "SCRAM-SHA-1",
+    },
+}}
 
 
 # Password validation
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+# Tworzenie modelu użytkownika
+class User(models.Model):
+    nick = models.CharField(max_length=100, blank=False,
+                            unique=True)
+    e_mail = models.EmailField(max_length=250,blank=False,
+                               unique=True)
+    phone_validator = RegexValidator(regex=r'^\+?1?\d{11}$',
+                                     message="Numer telefonu musi byc wprowadzony w formacie +XX XXX XXX XXX")
+    phone_number = models.CharField(validators=[phone_validator], max_length=12, blank=False)
+    join_date = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=20, blank=False)
+
+
+#Tworzenie modelu ulubionych
+class Favourites(models.Model):
+    owner = models.ForeignKey(
+        "auth.User",
+        related_name="recipes",
+        on_delete=models.CASCADE
+    )
+    ##################3#### SKOPIOWAĆ Z PRZEPISU RESZTĘ
